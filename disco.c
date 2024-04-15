296992033005f65c47debc8432413daabf4626da
G4br13L-J <gabriel.jimenez.navarro@est.una.ac.cr>
2024-04-14 22:20:48 -0600

    Cambios en disco.c

--- disco.c
+++ disco.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BYTES_TO_MB(bytes) ((bytes) / (1024 * 1024)) // Macro para convertir bytes a megabytes
+
 int main(int argc, char *argv[]) {
     if (argc == 1) {
         FILE *fp = fopen("/proc/mounts", "r");
@@ -11,11 +13,14 @@ int main(int argc, char *argv[]) {
         }
 
         char line[256];
-        fgets(line, sizeof(line), fp);
+        while (fgets(line, sizeof(line), fp) != NULL) {
+            char device[256], mountpoint[256], fs[256];
+            sscanf(line, "%s %s %s", device, mountpoint, fs);
+            if (strcmp(mountpoint, "/") == 0) {
+                break;
+            }
+        }
         fclose(fp);
-
-        char device[256], mountpoint[256], fs[256];
-        sscanf(line, "%s %s %s", device, mountpoint, fs);
 
         FILE *dfp = popen("df / | tail -n 1", "r");
         if (dfp == NULL) {
@@ -23,26 +28,31 @@ int main(int argc, char *argv[]) {
             return 1;
         }
 
-        unsigned long long total_size;
-        fscanf(dfp, "%*s %*s %*s %*s %llu", &total_size);
+        unsigned long long total_size = 0, used_space = 0, available_space = 0;
+        char output[512];
+        while (fgets(output, sizeof(output), dfp) != NULL) {
+            // Buscar los valores en la línea de salida utilizando expresiones regulares
+            if (sscanf(output, "%*s %*s %*s %llu %llu %llu", &total_size, &used_space, &available_space) == 3) {
+                break;
+            }
+        }
         pclose(dfp);
 
-        unsigned long long free_space;
-        sscanf(line, "%*s %*s %*s %*s %llu %*s", &free_space);
+        float utilization = ((float)used_space / total_size) * 100;
 
-        float utilization = 100.0 * free_space / total_size;
-
-        printf("Porcentaje total de espacio libre en disco: %.2f%%\n", utilization);
+        printf("Espacio utilizado en el disco: %lld MB\n", BYTES_TO_MB(used_space));
+        printf("Espacio disponible en el disco: %lld MB\n", BYTES_TO_MB(available_space));
+        printf("Porcentaje total de espacio utilizado en disco: %.2f%%\n", utilization);
     } else if (argc == 2 && (strcmp(argv[1], "-tm") == 0 || strcmp(argv[1], "-tu") == 0)) {
-        FILE *fp = popen("df -BM | grep '^/dev/' | awk '{print $4}'", "r");
-        if (fp == NULL) {
+        FILE *dfp = popen("df / | grep '^/' | awk '{print $5}'", "r");
+        if (dfp == NULL) {
             perror("Error al abrir el comando");
             return 1;
         }
 
         char output[20];
-        fgets(output, sizeof(output), fp);
-        pclose(fp);
+        fgets(output, sizeof(output), dfp);
+        pclose(dfp);
 
         printf("Porcentaje total de espacio libre en disco: %s", output);
     } else {
@@ -52,3 +62,4 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
